a0e49c76f75eaf8ab7d36f5a62f3e4c3b17a8d7a
Adity-aprasad <141990373+Adity-aprasad@users.noreply.github.com>
2024-12-18 22:23:05 +0530

    Create Themecolour.jsx

---
+++ theme/Themecolour.jsx
@@ -0,0 +1,18 @@
+const Themecolour = {
+    light: {
+        theme: 'light',
+        textColor: 'black',
+        background: 'white',
+        cardBackground: '#A865B5',
+        boxBackground: '#9E7BB5',
+    },
+    dark: {
+        theme: 'dark',
+        textColor: 'white',
+        background: 'black',
+        cardBackground: '#444',
+        boxBackground: '#555',
+    },
+};
+
+export default Themecolour;
